60db67ea04f68f6f84cbeace7df7ea2d58da3653
arthur2205cr <arthur2205cr@gmail.com>
2026-08-21 17:50:25 +0000

    buscarProdutoId

---
+++ rotas/buscarProdutoPorId.js
@@ -0,0 +1,17 @@
+import bd from "../bd.js";
+
+export default function buscarProdutoPorId(req, res) {
+    const { id } = req.params;
+
+    const query = 'SELECT id, id_fornecedor, nome, marca, tipo, descricao, valor, data_recebimento FROM produto WHERE id = ?';
+    
+    bd.query(query, [id], (erro, resultado) => {
+        if (erro)
+            return res.status(500).json({ msg_erro: erro.message});
+
+        if (resultado.length === 0)
+            return res.status(404).json({ msg_erro: `Nenhum livro encontrado no ID ${id}.` });
+
+        return res.json(resultado[0]);
+    });
+}
